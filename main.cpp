92ad2ba8712cecefa9ef2b748fac9350f2679329
lydia <lydia@strathmore.edu>
2024-09-23 18:07:45 +0300

    initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int number=0;
+    int sum=0;
+do {
+    sum+=number;
+    cout<<"Enter a number"<<endl;
+    cin>>number;
+}
+    while(number>=0);
+        cout<<"The sum is "<<sum<<endl;
+    return 0;
+    }
+
+
+
+
